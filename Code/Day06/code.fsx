c5f20eec570c738cf92c13975c79307ff35d6617
ijrussell <ijrussell@fastmail.co.uk>
2022-12-06 15:57:13 +0000

    list to array

--- Code/Day06/code.fsx
+++ Code/Day06/code.fsx
@@ -5,32 +5,32 @@ let data =
     |> File.ReadAllLines
     |> Seq.toList
 
-let findIndex (windowSize:int) (input:char list) =
+let findIndex (windowSize:int) (input:char[]) =
     let rec loop index acc rem =
         match acc, rem with
         | [], [] -> failwith "Unable to find a solution"
         | [], items -> 
             let window = items |> List.take windowSize
             let result = if window |> Set.ofList |> Set.count = windowSize then window else []
-            loop (index+1) (result@[]) items.Tail
+            loop (index+1) result items.Tail
         | _, _ -> index, acc // Solution found
-    loop (windowSize-1) [] input
+    loop (windowSize-1) [] (input |> Array.toList)
 
-// let findIndex (windowSize:int) (input:char list) =
+// let findIndex (windowSize:int) (input:char[]) =
 //     input
-//     |> List.windowed windowSize 
-//     |> List.findIndex (fun s -> s |> Set.ofList |> Set.count = windowSize)
+//     |> Seq.windowed windowSize 
+//     |> Seq.findIndex (fun s -> s |> Set.ofSeq |> Set.count = windowSize)
 //     |> (+) windowSize
 
 module Part1 =
 
     let results =
         data
-        |> List.map (fun s -> findIndex 4 (s.ToCharArray() |> Array.toList))
+        |> List.map (fun s -> findIndex 4 (s.ToCharArray()))
 
 module Part2 =
 
     let results =
         data
-        |> List.map (fun s -> findIndex 14 (s.ToCharArray() |> Array.toList))
+        |> List.map (fun s -> findIndex 14 (s.ToCharArray()))
 
