e2cfa408f02d6b26a424bed3e21bf9a2994def7f
ijrussell <ijrussell@fastmail.co.uk>
2022-12-08 12:03:57 +0000

    Part2 completed

--- Code/Day08/code.fsx
+++ Code/Day08/code.fsx
@@ -31,8 +31,46 @@ module Part1 =
         [
             for row in 0..lenRow-1 do
             for col in 0..lenCol-1 do
-                if newArray[row, col] then Some () else None
+                if newArray[row, col] then 1
         ]
-        |> List.choose id
         |> List.length
 
+module Part2 =
+
+    let scenicScore row col =
+        [
+            [for i in 0..col-1 do
+                if data[row, i] >= data[row, col] then ("W", 1, data[row, i]) else ("W", 0, data[row, i])] |> List.rev
+            [for i in col+1..lenCol-1 do
+                if data[row, i] >= data[row, col] then ("E", 1, data[row, i]) else ("E", 0, data[row, i])] 
+            [for i in 0..row-1 do
+                if data[i, col] >= data[row, col] then ("N", 1, data[i, col]) else ("N", 0, data[i, col])] |> List.rev 
+            [for i in row+1..lenRow-1 do
+                if data[i, col] >= data[row, col] then ("S", 1, data[i, col]) else ("S", 0, data[i, col])]
+        ]
+        |> List.concat
+        |> List.groupBy (fun (d,_,_) -> d)
+        |> List.map (fun (_,xs) -> 
+            xs
+            |> List.map (fun (_,score,_) -> score)
+            |> List.fold (fun acc item ->
+                let (count, go) = acc
+                if go && item = 0 then (count+1, true)
+                elif go && item = 1 then (count+1, false)
+                else (count, false)
+            ) (0, true)
+            |> fun (c,_) -> c
+        )
+        |> List.reduce (*)
+
+    let newArray = Array2D.init lenRow lenCol (fun row col -> scenicScore row col)
+
+    let maxScenicScore = 
+        [
+            for row in 1..lenRow-2 do
+            for col in 1..lenCol-2 do
+                newArray[row, col]
+        ]
+        |> List.max
+
+    let result = maxScenicScore
