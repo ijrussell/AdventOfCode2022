c9527f6fe99cfb3616d891da29711f32e51878a2
ijrussell <ijrussell@fastmail.co.uk>
2022-12-08 11:27:05 +0000

    refactor

--- Code/Day08/code.fsx
+++ Code/Day08/code.fsx
@@ -9,30 +9,30 @@ let data =
 let lenRow = data |> Array2D.length2 
 let lenCol = data |> Array2D.length1 
 
-let isVisible row col =
-    [
-        for i in 0..col-1 do
-            if data[row, i] >= data[row, col] then ("E", 1) 
-        for i in col+1..lenCol-1 do
-            if data[row, i] >= data[row, col] then ("W", 1) 
-        for i in 0..row-1 do
-            if data[i, col] >= data[row, col] then ("S", 1) 
-        for i in row+1..lenRow-1 do
-            if data[i, col] >= data[row, col] then ("N", 1) 
-    ]
-    |> List.groupBy (fun (d,_) -> d)
-    |> List.length < 4
+module Part1 =
 
-let outerTotal = 
-    ((lenCol - 1) + (lenRow - 1)) * 2
+    let isVisible row col =
+        [
+            for i in 0..col-1 do
+                if data[row, i] >= data[row, col] then ("E", 1) 
+            for i in col+1..lenCol-1 do
+                if data[row, i] >= data[row, col] then ("W", 1) 
+            for i in 0..row-1 do
+                if data[i, col] >= data[row, col] then ("S", 1) 
+            for i in row+1..lenRow-1 do
+                if data[i, col] >= data[row, col] then ("N", 1) 
+        ]
+        |> List.groupBy (fun (d,_) -> d)
+        |> List.length < 4
 
-let total = 
-    [
-        for row in 1..lenRow-2 do
-        for col in 1..lenCol-2 do
-            if isVisible row col then Some (row, col) else None
-    ]
-    |> List.choose id
-    |> List.length
-    |> fun t -> t + outerTotal
+    let newArray = Array2D.init lenRow lenCol (fun row col -> isVisible row col)
+
+    let total = 
+        [
+            for row in 0..lenRow-1 do
+            for col in 0..lenCol-1 do
+                if newArray[row, col] then Some () else None
+        ]
+        |> List.choose id
+        |> List.length
 
