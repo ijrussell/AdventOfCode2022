1e1d1156b720469ca9b61baacab4aefeffaa9c47
ijrussell <ijrussell@fastmail.co.uk>
2022-12-08 20:26:55 +0000

    simplify list comprehension

--- Code/Day08/code.fsx
+++ Code/Day08/code.fsx
@@ -42,13 +42,13 @@ module Part2 =
         [
             let cellValue = source[row, col]
             [for i in 0..col-1 do
-                if source[row, i] >= cellValue then ("W", 1, source[row, i]) else ("W", 0, source[row, i])] |> List.rev
+                ("W", (if source[row, i] >= cellValue then 1 else 0), source[row, i])] |> List.rev
             [for i in col+1..lenCol-1 do
-                if source[row, i] >= cellValue then ("E", 1, source[row, i]) else ("E", 0, source[row, i])] 
+                ("E", (if source[row, i] >= cellValue then 1 else 0), source[row, i])]
             [for i in 0..row-1 do
-                if source[i, col] >= cellValue then ("N", 1, source[i, col]) else ("N", 0, source[i, col])] |> List.rev 
+                ("N", (if source[i, col] >= cellValue then 1 else 0), source[i, col])] |> List.rev 
             [for i in row+1..lenRow-1 do
-                if source[i, col] >= cellValue then ("S", 1, source[i, col]) else ("S", 0, source[i, col])]
+                ("S", (if source[i, col] >= cellValue then 1 else 0), source[i, col])]
         ]
         |> List.concat
         |> List.groupBy (fun (d,_,_) -> d)
