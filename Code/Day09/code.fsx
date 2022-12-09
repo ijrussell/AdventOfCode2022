3373f0e2596269aa6691ea61bd7330b06fa79536
ijrussell <ijrussell@fastmail.co.uk>
2022-12-09 20:18:44 +0000

    Part2 completed

--- Code/Day09/code.fsx
+++ Code/Day09/code.fsx
@@ -5,7 +5,7 @@ let readLine (input:string) =
     (line[0], int line[1])
 
 let data = 
-    Path.Combine(__SOURCE_DIRECTORY__, "sample-data-2.txt")
+    Path.Combine(__SOURCE_DIRECTORY__, "data.txt")
     |> File.ReadAllLines
     |> Seq.toList
     |> List.map readLine
@@ -20,40 +20,27 @@ let makeMove (input:int * int) (direction:string) =
     | dir -> failwith $"Unknown direction: {dir}"
     
 let makeMoveTail (inputH:int * int) (inputT:int * int) =
-    let (hRow, hCol) = inputH
-    let (tRow, tCol) = inputT
-    let diffX, diffY = abs (hRow - tRow), abs (hCol - tCol) 
-    // covered
-    if inputH = inputT then inputT
-    // next to
-    elif (diffX = 0 && diffY = 1) || (diffX = 1 && diffY = 0) then inputT
-    // diagonal to
-    elif (diffX = 1 && diffY = 1) then inputT
-    // away in lane of travel
-    elif diffX = 0 && diffY = 2 && hCol > tCol then (tRow, tCol+1)
-    elif diffX = 0 && diffY = 2 && hCol < tCol then (tRow, tCol-1)
-    elif diffX = 2 && diffY = 0 && hRow > tRow then (tRow+1, tCol)
-    elif diffX = 2 && diffY = 0 && hRow < tRow then (tRow-1, tCol)
-    // away not in lane of travel
-    elif diffX = 1 && diffY = 2 && hCol > tCol && hRow > tRow then (tRow+1, tCol+1)
-    elif diffX = 1 && diffY = 2 && hCol > tCol && hRow < tRow then (tRow-1, tCol+1)
-    elif diffX = 1 && diffY = 2 && hCol < tCol && hRow > tRow then (tRow+1, tCol-1)
-    elif diffX = 1 && diffY = 2 && hCol < tCol && hRow < tRow then (tRow-1, tCol-1)
-   
-    elif diffX = 2 && diffY = 1 && hRow > tRow && hCol > tCol then (tRow+1, tCol+1)
-    elif diffX = 2 && diffY = 1 && hRow > tRow && hCol < tCol then (tRow+1, tCol-1)
-    
-    elif diffX = 2 && diffY = 1 && hRow < tRow && hCol > tCol then (tRow-1, tCol+1)
-    elif diffX = 2 && diffY = 1 && hRow < tRow && hCol < tCol then (tRow-1, tCol-1)
-    // else
-    else inputT
+    let (hRow, hCol), (tRow, tCol) = inputH, inputT
+    let diffX, diffY =
+        match hRow-tRow, hCol-tCol with
+        | 2, 0 -> 1, 0
+        | -2, 0 -> -1, 0
+        | 0, 2 -> 0, 1
+        | 0, -2 -> 0, -1
+        | 1, 2 | 2, 1 | 2, 2 -> 1, 1
+        | 2, -1 | 1, -2 | 2, -2 -> 1, -1
+        | -1, -2 | -2, -1 | -2, -2 -> -1, -1
+        | -2, 1 | -1, 2 | -2, 2 -> -1, 1
+        | x, y when abs x < 2 && abs y < 2 -> 0, 0
+        | ex -> failwith $"Unexpected values: {ex}"
+    (tRow + diffX, tCol + diffY)
 
 let processInstruction (current:int * int) (move:string * int) =
     let direction, distance = move
     [for _ in 1..distance do direction]
     |> List.fold (fun acc item ->
-        let current, history = acc
-        let latest = makeMove current item
+        let position, history = acc
+        let latest = makeMove position item
         latest, latest::history) (current, [])
     
 let calculateHead (directions:(string * int) list) =
@@ -63,6 +50,7 @@ let calculateHead (directions:(string * int) list) =
         let latest, latestMoves = processInstruction current item
         latest, latestMoves@history
     ) ((0, 0), [])
+    |> fun (_, items) -> items |> List.rev
 
 let calculateTail (positions:(int * int) list) =
     positions
@@ -71,26 +59,18 @@ let calculateTail (positions:(int * int) list) =
         let latest = makeMoveTail item current 
         latest, latest::history
     ) ((0,0), [])
+    |> fun (_, items) -> items |> List.rev
 
 let calculateTails (tails:string list) (positions:(int * int) list) =
     tails
-    |> List.fold (fun acc item -> 
-        let _, calculatedPositions = calculateTail acc
-        calculatedPositions
-    ) positions
+    |> List.fold (fun acc _ -> calculateTail acc) positions
 
 let result (tails:string list) = 
     data 
     |> calculateHead
-    |> fun (_,positions) -> positions
-    |> List.rev
     |> calculateTails tails
-    |> List.rev
+    |> List.distinct 
+    |> List.length
 
-//let lengthPart1 = result ["T"] //|> List.distinct |> List.length
-
-let lengthPart2 = [1..2] |> List.map string |> result // |> List.distinct |> List.length
-
-
-
-
+let part1 = ["T"] |> result 
+let part2 = [1..9] |> List.map string |> result 
